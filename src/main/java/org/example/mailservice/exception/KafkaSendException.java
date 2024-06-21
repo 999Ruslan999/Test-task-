d97ade0287cba47087e5eba874dcbb75c720f5fc
999Ruslan999 <113248592+999Ruslan999@users.noreply.github.com>
2024-06-21 18:54:35 +0300

    Delete src directory

--- src/main/java/org/example/mailservice/exception/KafkaSendException.java
+++
@@ -1,8 +0,0 @@
-package org.example.mailservice.exception;
-
-public class KafkaSendException extends RuntimeException{
-    public KafkaSendException(String message) {
-        super(message);
-    }
-}
-
